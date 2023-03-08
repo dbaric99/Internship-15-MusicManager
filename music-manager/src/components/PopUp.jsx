ca4771a8aa460648636f618d7db5acb7cfb4e0f0
dbaric99 <damjana.baric1999@gmail.com>
2023-03-08 23:57:30 +0100

    Opted out of using the material ui

--- music-manager/src/components/PopUp.jsx
+++ music-manager/src/components/PopUp.jsx
@@ -1,33 +1,24 @@
-import {Dialog, DialogContent, DialogTitle, Typography} from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
 import {PropTypes} from 'prop-types';
 import sprite from "../assets/images/sprite.svg";
 
-const useStyles = makeStyles({
-  albumFormTitle: {
-    color: 'var(--black)',
-    margin: '20px',
-    fontWeight: 'normal',
-    fontSize: '30px',
-  }
-})
-
 function PopUp({openPopUp, handleClose, title, children}) {
-  const classes = useStyles();
 
   return (
-    <Dialog className="pop-up" open={openPopUp}>
-      <DialogTitle className="pop-up__title-wrapper">
-        <Typography>{title}</Typography>
-        <svg className="pop-up__close-icon" width="20px" height="20px" onClick={() => handleClose(false)}>
-            <use href={`${sprite}#close-button`} />
-        </svg>
-      </DialogTitle>
+    <div>
+      
+    </div>
+    // <Dialog className="pop-up" open={openPopUp}>
+    //   <DialogTitle className="pop-up__title-wrapper">
+    //     <Typography>{title}</Typography>
+    //     <svg className="pop-up__close-icon" width="20px" height="20px" onClick={() => handleClose(false)}>
+    //         <use href={`${sprite}#close-button`} />
+    //     </svg>
+    //   </DialogTitle>
 
-      <DialogContent>
-        {children}
-      </DialogContent>
-    </Dialog>
+    //   <DialogContent>
+    //     {children}
+    //   </DialogContent>
+    // </Dialog>
   );
 }
 
--- music-manager/src/components/controls/Input.jsx
+++ music-manager/src/components/controls/Input.jsx
@@ -1,16 +1,9 @@
 import PropTypes from 'prop-types';
-import {TextField} from '@material-ui/core';
 
 function Input({label, name, value, onChange, showError, errorText}) {
+
   return (
-    <TextField
-      label={label} 
-      name={name} 
-      variant="standard"
-      value={value}
-      onChange={onChange}
-      {...(showError && {error:true,helperText:errorText})}
-    />
+    <div></div>
   )
 }
 
