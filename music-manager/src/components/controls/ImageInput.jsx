28cff4eef9c3b15895b15557d6cf7a955d3a46d3
dbaric99 <damjana.baric1999@gmail.com>
2023-03-10 17:08:21 +0100

    Added drag and drop functionality to album cover and allowed image preview

--- music-manager/src/components/controls/ImageInput.jsx
+++ music-manager/src/components/controls/ImageInput.jsx
@@ -4,6 +4,7 @@ import sprite from '../../assets/images/sprite.svg'
 
 function ImageInput() {
     const [file, setFile] = useState([]);
+    const [imageEl, setImageEl] = useState(null);
     const [isDropzoneFocused, setIsDropzoneFocused] = useState(false);
 
     const {getRootProps, getInputProps} = useDropzone({
@@ -15,19 +16,28 @@ function ImageInput() {
         console.log("DROPZONE:", acceptedFiles);
       }
     });
+
+    useEffect(() => {
+      if(!file.length) return;
+      console.log(file);
+      setImageEl(<img className="cover-preview" src={file[0].preview} alt="Cover art" style={{width:'70px', height: '70px'}}/>);
+    }, [file])
   
     return (
-      <div className='image-dropzone' {...getRootProps()}>
-          <input {...getInputProps()} />
-          <div className='dropzone-area' 
-            onMouseEnter={() => setIsDropzoneFocused(true)} 
-            onMouseLeave={() => setIsDropzoneFocused(false)}
-          >
-            <svg className='dropzone-add' width="50px" height="50px" >
-                <use href={!isDropzoneFocused ? `${sprite}#add` : `${sprite}#add-active`} />
-            </svg>
-          </div>
-      </div>
+      <>
+        <div className='image-dropzone' {...getRootProps()}>
+            <input {...getInputProps()} />
+            <div className='dropzone-area' 
+              onMouseEnter={() => setIsDropzoneFocused(true)} 
+              onMouseLeave={() => setIsDropzoneFocused(false)}
+            >
+              {imageEl && <div className={isDropzoneFocused ? 'cover-preview--hover' : ''}>{imageEl}</div> } 
+              <svg className='dropzone-add' width="50px" height="50px" >
+                  <use href={!isDropzoneFocused ? `${sprite}#add` : `${sprite}#add-active`} />
+              </svg>
+            </div>
+        </div>
+      </>
     );
 }
 
